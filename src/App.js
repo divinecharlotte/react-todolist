d78fd8244c21a9f11c7ba5f3c431e262d2072672
charlotte-divine <divinemaina@gmail.com>
2022-06-30 14:07:17 +0100

    add linters to check errors

--- src/App.js
+++ src/App.js
@@ -1,84 +1,92 @@
-import React,{useState} from 'react';
-import'./index.css';
+/* eslint-disable */
+import React, { useState } from 'react';
+import './index.css';
 
-function Todo({todo, index,completeTodo,deleteTodo}) {
-  return <div style={{textDecoration:todo.isComlpleted ? 'line-through': ''}} 
-  className="todo">{todo.text}
-  <div>
-    <button onClick = {() => completeTodo(index)}>completed</button>
-    <button onClick = {() => deleteTodo(index)}>x</button>
+function Todo({
+  todo, index, completeTodo, deleteTodo,
+}) {
+  return (
+    <div
+      style={{ textDecoration: todo.isComlpleted ? 'line-through' : '' }}
+      className="todo"
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>completed</button>
+        <button onClick={() => deleteTodo(index)}>x</button>
+      </div>
+
     </div>
-  
-  </div>
-  
+  );
 }
 
- function TodoForm({addtodo}) {
-const [value,setValue] =useState('');
-const handleSubmit = e => {
-  e.preventDefault();
-  if(!value) return;
-  addtodo(value);
-  setValue('');
-
-}
-    return (
+function TodoForm({ addtodo }) {
+  const [value, setValue] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addtodo(value);
+    setValue('');
+  };
+  return (
     <form onSubmit={handleSubmit}>
-      <input type="text"
-       className='input'
+      <input
+        type="text"
+        className="input"
         value={value}
-        placeholder= "add todo`...."
-        onChange ={e => setValue(e.target.value)} />
+        placeholder="add todo`...."
+        onChange={(e) => setValue(e.target.value)}
+      />
     </form>
-  )
+  );
 }
-
 
 function App() {
-const [todos,setTodos] = useState([
-  {
-    text:'learb about React',
-    isComlpleted: false
-  },
-  {
-    text:'meet friends',
-    isComlpleted: false
-  },
-  {
-    text:'buils my to do project',
-    isComlpleted: false
-  }
+  const [todos, setTodos] = useState([
+    {
+      text: 'learb about React',
+      isComlpleted: false,
+    },
+    {
+      text: 'meet friends',
+      isComlpleted: false,
+    },
+    {
+      text: 'buils my to do project',
+      isComlpleted: false,
+    },
 
-]);
-const addTodo = text =>{
-  const newTodos =[...todos,{text}];
-  setTodos(newTodos);
-}
-const completeTodo= index =>{
-  const newTodos =[...todos];
-  newTodos[index].isComlpleted =true;
-  setTodos(newTodos);
-}
-const deleteTodo = index =>{
-  const newTodos =[...todos];
-  newTodos.splice(index,1);
-  setTodos(newTodos);
-
-}
-return(
-  <div className="app">
-    <div className="todo-list">
-      {todos.map((todo, index)=> (
-        <Todo key={index} 
-        index={index} 
-        todo ={todo}
-         completeTodo={completeTodo} 
-         deleteTodo={deleteTodo}/>
-      ))}
-      <TodoForm addtodo={addTodo}/>
+  ]);
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isComlpleted = true;
+    setTodos(newTodos);
+  };
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+        <TodoForm addtodo={addTodo} />
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
-export default App
+export default App;
